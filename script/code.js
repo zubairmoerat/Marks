b931a0372435c0336b842f6619b48283a440d826
zubairmoerat <zubairmoeratofficial@gmail.com>
2023-11-13 12:33:31 +0200

    getItDone

---
+++ script/code.js
@@ -0,0 +1,12 @@
+document.getElementById('calculateGrade').addEventListener("click",function(){
+    const marks = parseFloat(document.getElementById('marks').value)
+    const btnSubmit = parseFloat(document.getElementById('btnSubmit').alert)
+})
+switch (marks) {
+    case 100:
+        alert("A+")
+    break;
+    case 90<=99:
+        alert("A")
+    break;
+}
